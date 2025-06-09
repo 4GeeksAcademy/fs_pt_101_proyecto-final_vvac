6bdd2e73fbc1467c2a1f847a1a71f8a8630f4faa
Vbarbosa <131600563+VitoriaBSouza@users.noreply.github.com>
2025-06-09 17:28:33 +0000

    pull from develop

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -26,13 +26,10 @@ export const router = createBrowserRouter(
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-        <Route path= "/" element={<Home />} />
-<<<<<<< HEAD
+git        <Route path= "/" element={<Home />} />
         <Route path= "/LogIn" element={<LogIn />} />
         <Route path= "/Register" element={<Register />} />
-=======
         <Route path="/recipes/:id" element={<RecipeDetails />} />
->>>>>>> 01690acfbc95836eb03a37925233d3018827796a
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
       </Route>
